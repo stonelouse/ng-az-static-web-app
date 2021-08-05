49580f66a886f29cdb14ed68358a250796fb5961
Christian Russ <russ.c@eplan.de>
2021-08-05 13:15:00 +0200

    feature: JS azure-function called by ng-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  template: `<div>{{value}}</div>`,
 })
 export class AppComponent {
   value = 'World';
+
+  constructor(private readonly http: HttpClient) {
+    this.http.get('api/message?name=ng').subscribe((resp: any) => {
+      console.log('#01', {resp});
+      return this.value = resp.text;
+    });
+  }
 }
